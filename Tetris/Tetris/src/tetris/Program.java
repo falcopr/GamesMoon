be1aa85edcfc4ff7e178dc4182d72727f38937d5
falcoprescher <falcoprescher@googlemail.com>
2012-12-02 15:06:48 +0100

    Ist nun auch als Desktopanwendung benutzbar

--- Tetris/Tetris/src/tetris/Program.java
+++ Tetris/Tetris/src/tetris/Program.java
@@ -2,6 +2,8 @@ package tetris;
 
 import java.applet.Applet;
 
+import javax.swing.JFrame;
+
 import tetris.businesslogic.container.BusinessLogicContainer;
 import tetris.common.ServerConnector;
 import tetris.presenter.container.PresenterContainer;
@@ -14,8 +16,20 @@ public class Program extends Applet {
 	ServerConnector connector;
 
 	public static void main(String[] args) {
-		// framework should handle the right time to create GUI
-		// javax.swing.SwingUtilities.invokeLater(null);
+		ViewContainer.initializeToProductiveContainer();
+		PresenterContainer.initializeToProductiveContainer();
+		BusinessLogicContainer.initializeToProductiveContainer();
+
+		IPlayingAreaView startingView = ViewContainer.getViewContainer().getComponent(IPlayingAreaView.class); 
+		
+		JFrame f = new JFrame("Test");
+		 f.add("Center", startingView.getPlayingAreaPanel());
+		 f.setSize(500, 650);
+		
+		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 f.pack();
+		 f.setResizable(false);
+		 f.setVisible(true);
 	}
 
 	@Override
